874ce0dd5b5f028353609a8695fd3d77fb17c941
Egor Alekseyevich Morozov <zozo5120@outlook.com>
2026-04-28 19:39:48 +0800

    Add F1 Score

--- model/metric.py
+++ model/metric.py
@@ -1,20 +1,13 @@
 import torch
 
 
-def accuracy(output, target):
+def f1_score(output, target):
     with torch.no_grad():
         pred = torch.argmax(output, dim=1)
         assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
-
-
-def top_k_acc(output, target, k=3):
-    with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
+        tp = torch.sum((pred == 1) & (target == 1)).item()
+        fp = torch.sum((pred == 1) & (target == 0)).item()
+        fn = torch.sum((pred == 0) & (target == 1)).item()
+        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+        recall = tp / (tp + fn) if (tp + fn) > 0 else 0
+    return 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
