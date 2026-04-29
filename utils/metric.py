3e5fd9987356b61e3b498d7b426bf709ce27b4b8
Egor <611221201@gms.ndhu.edu.tw>
2026-04-29 13:31:06 +0800

    Add metrics

--- utils/metric.py
+++ utils/metric.py
@@ -1,5 +1,24 @@
 import torch
 
+def precision(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        tp = torch.sum((pred == 1) & (target == 1)).item()
+        fp = torch.sum((pred == 1) & (target == 0)).item()
+        precision = tp / (tp + fp) if (tp + fp) > 0 else 0
+        return precision
+
+
+def recall(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        tp = torch.sum((pred == 1) & (target == 1)).item()
+        fp = torch.sum((pred == 1) & (target == 0)).item()
+        recall = tp / (tp + fp) if (tp + fp) > 0 else 0
+        return recall
+
 
 def f1_score(output, target):
     with torch.no_grad():
@@ -11,3 +30,11 @@ def f1_score(output, target):
         precision = tp / (tp + fp) if (tp + fp) > 0 else 0
         recall = tp / (tp + fn) if (tp + fn) > 0 else 0
     return 2 * precision * recall / (precision + recall) if (precision + recall) > 0 else 0
+
+
+def hitsat1(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = torch.sum(pred == target).item()
+        return correct / len(target)
